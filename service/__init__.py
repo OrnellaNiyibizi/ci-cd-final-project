63517a6f4c6cb7b41ea04fecd4d507b45ea6f2f7
Ornella Mahoro <109486545+OrnellaNiyibizi@users.noreply.github.com>
2024-11-08 13:37:35 +0200

    ci/cd

---
+++ service/__init__.py
@@ -0,0 +1,20 @@
+"""
+Service Package
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+# This must be imported after the Flask app is created
+# pylint: disable=wrong-import-position,cyclic-import
+from service import routes
+# pylint: disable=wrong-import-position
+from service.common import (
+    log_handlers
+)
+
+log_handlers.init_logging(app, "gunicorn.error")
+
+app.logger.info(70 * "*")
+app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(70 * "*")
